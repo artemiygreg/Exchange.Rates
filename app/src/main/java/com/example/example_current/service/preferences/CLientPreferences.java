bd4749da418d6995efc130d33fe34bb873f64a62
Artem.Grigorovich <artemiygreg@mail.ru>
2015-08-27 13:59:20 +0400

    upgrade, implemented material designe, refactoring code

---
+++ app/src/main/java/com/example/example_current/service/preferences/CLientPreferences.java
@@ -0,0 +1,11 @@
+package com.example.example_current.service.preferences;
+
+/**
+ * Created by Admin on 25.08.15.
+ */
+public class CLientPreferences {
+
+    public CLientPreferences() {
+
+    }
+}
